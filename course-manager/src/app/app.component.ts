b32c6454756d72b245f15a7b640b03bd61027fbc
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2023-01-01 19:12:14 -0300

    Atualizando o projeto com bootstrap

--- course-manager/src/app/app.component.ts
+++ course-manager/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-manager';
+  title = 'Gerenciador de cursos'
+
+  name: string = 'Adalberto'
 }
